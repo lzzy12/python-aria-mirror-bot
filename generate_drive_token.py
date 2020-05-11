f9de9595fa4d0c4f7a76b35881fe20e4be1d3405
W4RR10R <46273006+CW4RR10R@users.noreply.github.com>
2020-05-11 14:27:52 +0530

    Get rid of token.pickle 😬

--- generate_drive_token.py
+++ generate_drive_token.py
@@ -1,22 +1,26 @@
-import pickle
-import os
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+from oauth2client.client import OAuth2WebServerFlow
 
-credentials = None
-__G_DRIVE_TOKEN_FILE = "token.pickle"
-__OAUTH_SCOPE = ["https://www.googleapis.com/auth/drive"]
-if os.path.exists(__G_DRIVE_TOKEN_FILE):
-    with open(__G_DRIVE_TOKEN_FILE, 'rb') as f:
-        credentials = pickle.load(f)
-        if credentials is None or not credentials.valid:
-            if credentials and credentials.expired and credentials.refresh_token:
-                credentials.refresh(Request())
-else:
-    flow = InstalledAppFlow.from_client_secrets_file(
-        'credentials.json', __OAUTH_SCOPE)
-    credentials = flow.run_console(port=0)
+# Why my "App isn't verified" ?
+# This might returned by google APIs because we are using a high level scope here 
+# How to fix it ?
+# Just hit the continue anyway button because here you are using you own credentials so no one gonna steal your data
+# else complete your developer/app profile and submit for review and get verified
+# W4RR10R
 
-# Save the credentials for the next run
-with open(__G_DRIVE_TOKEN_FILE, 'wb') as token:
-    pickle.dump(credentials, token)
+__OAUTH_SCOPE = ['https://www.googleapis.com/auth/drive']
+__REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
+
+__CLIENT_ID = input("Enter the client id: ")
+__CLIENT_SECRET = input("Enter the client secret: ")
+
+flow = OAuth2WebServerFlow(
+        __CLIENT_ID,
+        __CLIENT_SECRET,
+        __OAUTH_SCOPE,
+        redirect_uri=__REDIRECT_URI
+        )
+auth_url = flow.step1_get_authorize_url()
+print("Open this URL in any browser and get the refersh token: \n" + auth_url)
+refresh_token = input("Enter the Refresh token: ")
+auth = flow.step2_exchange(refresh_token).to_json()
+print(auth)
