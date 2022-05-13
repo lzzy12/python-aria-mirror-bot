8dee90b18d28856009044d4c34bb8676a8df0b41
Shubham Gopale <shubham.gopale@gojek.com>
2022-05-13 09:12:53 +0530

    fix drive token generation and update readme
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- generate_drive_token.py
+++ generate_drive_token.py
@@ -9,13 +9,17 @@ __OAUTH_SCOPE = ["https://www.googleapis.com/auth/drive"]
 if os.path.exists(__G_DRIVE_TOKEN_FILE):
     with open(__G_DRIVE_TOKEN_FILE, 'rb') as f:
         credentials = pickle.load(f)
-        if credentials is None or not credentials.valid:
-            if credentials and credentials.expired and credentials.refresh_token:
-                credentials.refresh(Request())
+        if (
+            (credentials is None or not credentials.valid)
+            and credentials
+            and credentials.expired
+            and credentials.refresh_token
+        ):
+            credentials.refresh(Request())
 else:
     flow = InstalledAppFlow.from_client_secrets_file(
         'credentials.json', __OAUTH_SCOPE)
-    credentials = flow.run_console(port=0)
+    credentials = flow.run_local_server(port=0)
 
 # Save the credentials for the next run
 with open(__G_DRIVE_TOKEN_FILE, 'wb') as token:
